888212a819ecea6d5a1b87cc38397b6101825249
OtabekDeveloper <otabekahmadjonovvv@gmail.com>
2022-10-05 18:01:18 +0500

    bar

--- src/category/model.js
+++ src/category/model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const CategorySchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, minlength: 4, maxlength: 50 },
+    title: { type: String, required: true, minlength: 4, maxlength: 50 },
   },
   { versionKey: false }
 );
