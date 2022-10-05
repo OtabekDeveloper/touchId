b9c8fed8699f7345957b14d2e5b4d56514ea54c1
OtabekDeveloper <otabekahmadjonovvv@gmail.com>
2022-10-05 21:51:18 +0500

    bla

--- src/room/model.js
+++ src/room/model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const RoomSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, minlength: 4, maxlength: 50 },
+    title: { type: String, required: true, minlength: 4, maxlength: 50 },
   },
   { versionKey: false }
 );
