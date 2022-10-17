c321a7eb1eea86ceb07be1803b0d28fbbc02d0d7
OtabekDeveloper <52341708+OtabekDeveloper@users.noreply.github.com>
2022-10-17 14:04:18 +0500

    Update controller.js

--- src/device/controller.js
+++ src/device/controller.js
@@ -62,8 +62,9 @@ module.exports = {
         const doc = await Device.findById(req.params.id).exec();
         if(doc?.parent){
           await Parametr.deleteMany({subCategory : doc._id})
+          await Device.findByIdAndDelete(doc._id)
         }else {
-          await Parametr.findByIdAndDelete(doc._id)
+          await Device.findByIdAndDelete(doc._id)
         }
         if (!doc) throw new Error();
         return res.status(200).json({ _id: doc._id });
